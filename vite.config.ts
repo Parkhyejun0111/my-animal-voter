dfa4856ff5a3946aa153d2af50de554b012f5bbb
hyejun <qkzpwns0111@gmail.com>
2026-07-07 15:00:53 +0900

    fix: vite config base 경로를 my-animal-voter로 수정

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/my_uiux_01/', 
+  base: '/my-animal-voter/', 
   plugins: [react()],
 })
